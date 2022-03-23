a19860372dee286a4a3b4289b39b702f84d7cc01
David MANUEL temp <davidmbmanuel@gmail.com>
2022-03-23 16:42:06 +0100

    site affichant température

--- serverHTTP/serverHTTP.ino
+++ serverHTTP/serverHTTP.ino
@@ -1,6 +1,22 @@
 #include <SPI.h> // Libairie communication SPI avec le Shield éthernet
 #include <Ethernet.h> // Librairie communication Ethernet sur un réseau local
+#include <Wire.h>
+#include "rgb_lcd.h"
+#include <Adafruit_BME280.h> 
 
+//Constante écran
+rgb_lcd lcd;
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+
+//Constante Capteur 
+
+#define adresseI2CduBME280                0x76            // Adresse I2C du capteur
+#define pressionAuNiveauDeLaMerEnHpa      1024.90    
+#define delaiRafraichissementAffichage    1500            // Délai de rafraîchissement de l'affichage (en millisecondes)
+
+Adafruit_BME280 bme;
 //On donne l'adresse MAC au shield Ethernet (ne doit pas être identique à une autre adresse MAC présente sur votre réseau)
 byte MAC[6] = { 0xA8, 0x61, 0x0A, 0xAE, 0x17, 0x0E }; // Adresse MAC du shield Ethernet
 String request; 
@@ -12,6 +28,10 @@ EthernetServer server(80); // Le Shield devient un serveur sur le port 80 (port 
 EthernetClient client;
 void setup()
   {
+
+    bme.begin(adresseI2CduBME280);
+    lcd.begin(16, 2);
+    lcd.setRGB(colorR, colorG, colorB);
     
   //Lancement connexion Ethernet avec l'adresse MAC et l'adresse IP
   Serial.begin(9600);
@@ -26,11 +46,15 @@ void setup()
 
 void loop(){
  client = server.available();
+
+  
  clientRequest();
  handleRequest();
+ delay(500);
+ 
   }
 
-  void clientRequest( ) {
+  void clientRequest() {
  if (!client) {
    return;
  }
@@ -55,6 +79,10 @@ void handleRequest( ) {
  }
 }
 void webpage(EthernetClient client) { /* function webpage */
+ lcd.setCursor(0, 1);
+ float t=bme.readTemperature();
+
+ lcd.print(t);
  client.println(F("HTTP/1.1 200 OK"));
  client.println(F("Content-Type: text/html"));
  client.println();
@@ -65,7 +93,10 @@ void webpage(EthernetClient client) { /* function webpage */
  client.print(F("<br><hr/><hr><h1 style='color : #3AAA35;'><center> Paramètres du Variateur d'ambiance </center></h1>"));
  client.print(F("<hr/><hr><h2 style='color:green;'>Données récoltées : </h2>"));
  client.print(F("<h1 style='color : #3AAA35;'><center> Paramètres du Variateur d'ambiance </center></h1>"));
- client.print(F("<p style='color:white;'><ul><li style ='color:white;'>Température relevée : X°C</li>"));
+ client.print(F("<p style='color:white;'><ul><li style ='color:white;'>Température relevée : "));
+ client.print(t);
+ client.print(F("°C</li>"));
+
  client.print(F("<li style ='color:white;'>Musique actuellement jouée : blabla</li><li style ='color:white;'>texte affiché : blabla</li>"));
  client.print(F("</ul></p><br><h2 style='color:green;'>Configuration actuelle : </h2>"));
  client.print(F("<ul><li style ='color:white;'>Prochaine Musique : </li></ul>"));
