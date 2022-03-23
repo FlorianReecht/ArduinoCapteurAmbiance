cf1cb0f993ffe2b69e9278e214671a9c70e3dae4
David MANUEL temp <davidmbmanuel@gmail.com>
2022-03-23 16:13:16 +0100

    correction emplacement ArduinoAmbiance

--- serverHTTP/serverHTTP.ino
+++ serverHTTP/serverHTTP.ino
@@ -4,7 +4,6 @@
 //On donne l'adresse MAC au shield Ethernet (ne doit pas être identique à une autre adresse MAC présente sur votre réseau)
 byte MAC[6] = { 0xA8, 0x61, 0x0A, 0xAE, 0x17, 0x0E }; // Adresse MAC du shield Ethernet
 String request; 
-unsigned long refreshCounter = 0;
 //On affecte l'adresse IP du Shield Ethernet
 IPAddress IP(169,254,152, 92); // Adresse IP donné au Shield Ethernet
 
@@ -18,11 +17,6 @@ void setup()
   Serial.begin(9600);
   Serial.println(F("Initialize System"));
   Ethernet.begin(MAC, IP);
-//  while (!Ethernet.begin(MAC)) {
-//    Serial.println(F("failed. Retrying in 1 second."));
-//    delay(1000);
-//    Serial.print(F("Starting W5500..."));
-//  }
   pinMode(2,OUTPUT);
   server.begin();
   Serial.println(F("W5500 initialized"));
@@ -36,51 +30,19 @@ void loop(){
  handleRequest();
   }
 
-  void clientRequest( ) { /* function clientRequest */
- ////Get client request
+  void clientRequest( ) {
  if (!client) {
    return;
  }
- // Wait until the client sends some data
  while (!client.available()) {
    delay(1);
  }
- request = client.readStringUntil('\r'); // Read the first line of the request
+ request = client.readStringUntil('\r');
  Serial.println(request);
  client.flush();
-}client.print(F("<!DOCTYPE HTML><html><head><title>Variateur d'ambiance</title>"));
- client.print(F("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>"));
- //meta-refresh page every x seconds
- client.print(F("<meta http-equiv='refresh' content='5'>"));
- client.print(F("</head><body bgcolor='black'><br>"));
- client.print(F("<hr/><hr>"));
- client.print(F("<h1 style='color : #3AAA35;'><center> Paramètres du Variateur d'ambiance </center></h1>"));
- client.print(F("<hr/><hr>"));
- client.print(F("<h2 style='color:green;'>Données récoltées : </h2>"));
- client.println("<p style='color:white;'>");
- client.print(F("<br><br>"));
- client.print("<br><br>");
- for (int i = 0; i < 6; i++) {
-   client.print("<b>Input A");
-   client.print(i);
-   client.print(" : </b>");
-   client.print(analogRead(14 + i)); //A0=14, A1=15 ,etc.
-   client.print(F("<br>"));
- }
- client.print(F("</p><br>"));
- client.print(F("<h2 style='color:green;'>Paramètres : </h2>"));
- client.print(F("<p style='color:white;'>"));
- //digital output
- client.print(F("<br><br>"));
- client.print(F("<b>Digital output Pin 2 : </b>"));
- client.print("<input value=" + String(digitalRead(2)) + " readonly></input>");
- client.print(F("<a href='/dig2on'><button>Turn On </button></a>"));
- client.print(F("<a href='/dig2off'><button>Turn Off </button></a><br />"));
- client.print(F("</p><br>"));
- //file end
- client.print(F("<br></body></html>"));
-void handleRequest( ) { /* function handleRequest */
- ////Handle web client request
+ 
+}
+void handleRequest( ) { 
  if (request.indexOf("/dig2on") > 0) {
    digitalWrite(2, HIGH);
  }
@@ -93,12 +55,28 @@ void handleRequest( ) { /* function handleRequest */
  }
 }
 void webpage(EthernetClient client) { /* function webpage */
- ////Send webpage to client
- //output HTML data header
  client.println(F("HTTP/1.1 200 OK"));
  client.println(F("Content-Type: text/html"));
  client.println();
- //header
+
+ client.print(F("<!DOCTYPE HTML><html><head><title>Variateur d'ambiance</title>"));
+ client.print(F("<meta http-equiv='content-type' content='text/html; charset=UTF-8'/>"));
+ client.print(F("<meta http-equiv='refresh' content='5'/></head><body bgcolor='black'>"));
+ client.print(F("<br><hr/><hr><h1 style='color : #3AAA35;'><center> Paramètres du Variateur d'ambiance </center></h1>"));
+ client.print(F("<hr/><hr><h2 style='color:green;'>Données récoltées : </h2>"));
+ client.print(F("<h1 style='color : #3AAA35;'><center> Paramètres du Variateur d'ambiance </center></h1>"));
+ client.print(F("<p style='color:white;'><ul><li style ='color:white;'>Température relevée : X°C</li>"));
+ client.print(F("<li style ='color:white;'>Musique actuellement jouée : blabla</li><li style ='color:white;'>texte affiché : blabla</li>"));
+ client.print(F("</ul></p><br><h2 style='color:green;'>Configuration actuelle : </h2>"));
+ client.print(F("<ul><li style ='color:white;'>Prochaine Musique : </li></ul>"));
+ client.print(F("<br><h2 style='color:green;'>Paramètres : </h2>"));
+ client.print(F("<p style='color:white;'><br><br><b>Nouveau texte à afficher</b>"));
+ client.print(F("</p><br>"));
+ client.print(F("<h2 style='color:green;'>Paramètres : </h2>"));
+ client.print(F("<p style='color:white;'>"));
+
+ client.print(F("<input ></input><a href='/dig2on'><button>Turn On </button></a><a href='/dig2off'><button>Turn Off </button></a><br />"));
+ client.print(F("</p><br><br></body></html>"));
  
  delay(1);
 }
